dd79c8414d942d499839b0cf72d7376e1ff4675f
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2024-10-21 12:55:13 +0200

    debut 2.6

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < Sinatra::Base
   get '/' do
-    erb :index
+    erb :index, locals: {gossips: Gossip.all}
   end
   get '/gossips/new' do
     erb :new_gossip
   end    
   post '/gossips/new/' do
     Gossip.new(params["gossip_author"],params["gossip_content"]).save
+    redirect '/'
   end
   
 end
