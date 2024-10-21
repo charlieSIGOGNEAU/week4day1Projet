8eddacd62e262237082539dc72c84433b3a66ffd
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2024-10-21 14:15:09 +0200

    debut 2.7

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,10 +4,20 @@ class ApplicationController < Sinatra::Base
   end
   get '/gossips/new' do
     erb :new_gossip
-  end    
+  end 
+  get '/gossips/:id' do
+    all_gossips = Gossip.all
+    gossip_id = params['id'].to_i
+    if gossip_id <= all_gossips.length && gossip_id > 0
+      gossip = all_gossips[gossip_id-1]
+      erb :show_gossip, locals: { gossip: gossip }
+    else
+      "Gossip not found"
+    end
+  end
+
   post '/gossips/new/' do
     Gossip.new(params["gossip_author"],params["gossip_content"]).save
     redirect '/'
-  end
-  
+  end  
 end
